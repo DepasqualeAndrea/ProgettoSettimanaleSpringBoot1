fa005fad7db8ab4f19e095c9b2dca29a7ecbf131
Andrea De Pasquale <128024931+DepasqualeAndrea@users.noreply.github.com>
2023-07-28 12:26:48 +0200

    progetto Settimanale inizio

---
+++ ProgettoSetiamnaleSpringBoot1/ProgettoSetiamnaleSpringBoot1/src/main/java/depasqaleRepository/ProgettoSetiamnaleSpringBoot1/ProgettoSetiamnaleSpringBoot1Application.java
@@ -0,0 +1,13 @@
+package depasqaleRepository.ProgettoSetiamnaleSpringBoot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSetiamnaleSpringBoot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoSetiamnaleSpringBoot1Application.class, args);
+	}
+
+}
---
+++ ProgettoSetiamnaleSpringBoot1/ProgettoSetiamnaleSpringBoot1/src/test/java/depasqaleRepository/ProgettoSetiamnaleSpringBoot1/ProgettoSetiamnaleSpringBoot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package depasqaleRepository.ProgettoSetiamnaleSpringBoot1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSetiamnaleSpringBoot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
